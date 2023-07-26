8b61c36fe5843c87efbb9419116e59a122464f43
Nataliya Trushina <nitrushina@hotmail.com>
2023-07-26 20:14:34 +0200

    Almost working function for quick filtering with awk

--- ProteinDatabaseCleanup.sh
+++ ProteinDatabaseCleanup.sh
@@ -3,11 +3,11 @@
 # cd /mnt/c/Users/NITru/OneDrive/Documents/PhD_work/Projects/NT_projects/ProteinDatabaseCleanup/MouseProteomeCleanup
 
 # Variables
-INPUT="cleaned_before_dedup.fasta"
-OUTPUT_DEDUP="deduped.fasta"
-OUTPUT="cured_mouse_database.fasta"
+INPUT="uniprot_proteome_mouse.fasta"
+OUTPUT="prepared_mouse_database.fasta"
 LOG_FILE="sequence_counts.log"
 REMOVED="removed_sequences.fasta"
+FINAL_OUTPUT="final_output.fasta"
 
 # Get the start time
 START_TIME=$(date +%s)
@@ -23,74 +23,69 @@ log_counts() {
 
 # This function linearizes the fasta input and prints to stdout
 linearize_fasta() {
-  awk '/^>/ {printf("%s%s\n",(N>0?"\n":""),$0);N++;next;}
-     {printf("%s",$0);}
-  END  {printf("\n");}' < "$1"
+  awk '/^>/ {printf("%s%s\n",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' < "$1"
 }
 
-# This function prepares sequences, removes sequences 
-# without GN ids OR uncharacterized OR fragmented sequences
-# OR sequences from pseudogenes.
-# It also logs sequences that were removed.
+# This function prepares sequences, removes sequences without GN ids OR uncharacterized OR fragmented sequences
+# OR sequences from pseudogenes. It also logs sequences that were removed.
 prepare_sequences() {
+  local input_fasta=$1
+  local output_fasta=$2
+  local removed_fasta=$3
+
   local lin_fasta_file=$(mktemp)
-  linearize_fasta "$1" > "$lin_fasta_file"
-  awk 'BEGIN {RS = ">" ; FS = "\n" ; ORS = ""} 
-       $2 !~ /GN=|[uU]ncharacterized|(Fragment)|pseudogene/ && length($2) > 50 {print ">"$0 > "'"$2"'";}
-       $2 ~ /GN=|[uU]ncharacterized|(Fragment)|pseudogene/ || length($2) <= 50 {print ">"$0 > "'"$3"'";}' "$lin_fasta_file"
+  linearize_fasta "$input_fasta" > "$lin_fasta_file"
+
+  # Remove extra '>' characters from the beginning of the sequence header
+  sed -i 's/^>>*>/>/g' "$lin_fasta_file"
+
+  # Remove '->' from headers
+  sed -i 's/->//g' "$lin_fasta_file"
+
+  awk 'BEGIN {RS = "\n>"; FS = "\n"; ORS = "\n"} 
+       {
+         if ($1 ~ /GN=/ && $1 !~ /uncharacterized|Fragment|pseudogene/ && length($2) > 50) {
+           print ">"$0 > "'"$output_fasta"'"
+         } else {
+           print ">"$0 > "'"$removed_fasta"'"
+         }
+       }' "$lin_fasta_file"
+
   rm "$lin_fasta_file"
 }
 
-sort_sequences_by_length() {
+# This function sorts sequences by length, SwissProt status, and gene names. It also deduplicates sequences.
+sort_and_deduplicate_sequences() {
   local input_fasta=$1
   local output_fasta=$2
 
-  # This transforms the FASTA file so that each sequence is on a single line, prefixed by the header
-  # Then, it adds sequence length at the beginning of each line
-  # Finally, it sorts the sequences by length in reverse order (longest first) and removes the lengths
-  awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);}' "$input_fasta" |\
-  awk -F '\t' '{printf("%d\t%s\n",length($2),$0);}' |\
-  sort -k1,1nr | cut -f 2- | tr "\t" "\n" > "$output_fasta"
-}
-
-deduplicate_sequences() {
-  local fasta_file=$1
-  local dedup_file=$2
-  
-  # Sort sequences by length, SwissProt, and then gene name
-  local sorted_file=$(mktemp)
-  sort_fasta_by_gene "$fasta_file" "$sorted_file"
-  
-  # Initialize previous gene name
-  local prev_gene=""
-  
-  # Get total number of lines (for progress bar)
-  local total_lines=$(wc -l < "$sorted_file")
-  local line_counter=0
-  
-  # Process sorted sequences
-  while IFS= read -r line; do
-    # Extract gene name from the current sequence
-    local current_gene=$(echo "$line" | awk -F '|' '{print $5}')
-    
-    # If the current gene is different from the previous gene, this is a new group
-    if [[ "$current_gene" != "$prev_gene" ]]; then
-      # Select the sequence and append it to the output file
-      echo "$line" >> "$dedup_file"
-      
-      # Update the previous gene name
-      prev_gene="$current_gene"
-    fi
-    
-    # # Update progress bar
-    # line_counter=$((line_counter+1))
-    # if ((line_counter % 1000 == 0)); then
-    #   echo -ne "Progress: $((100*line_counter/total_lines))%\r"
-    # fi
-  done < "$sorted_file"
-  echo  # Move to the new line after the progress bar
-  
-  rm "$sorted_file"
+  gawk '/^>/ { gsub(" ","*"); if (seq) { print header; print seq } header=$0; seq="" } !/^>/ { seq = seq $0 } END { print header; print seq }' "$input_fasta" |\
+  awk -v RS='>' -v ORS='' '
+  {
+      if(NR>1) {
+          header=$1; sub(/^[^\n]*\n/, "", $0); seq=$0
+          if(header ~ /^sp\|/){
+              db = "sp"
+          }else if(header ~ /^tr\|/){
+              db = "tr"
+          }else{
+              db = "other"
+          }
+          len = length(seq)
+          if(!(header in headers)) {
+            data[db, header, -len] = seq
+            headers[header]
+          }
+      }
+  }
+  END {
+      PROCINFO["sorted_in"] = "@ind_str_asc"
+      for (ind in data) {
+          split(ind, arr, SUBSEP)
+          gsub("*"," ",arr[2])
+          print ">" arr[2] "\n" data[ind]
+      }
+  }' > "$output_fasta"
 }
 
 # Call the functions
@@ -98,16 +93,52 @@ log_counts "$INPUT" "Original count"
 prepare_sequences "$INPUT" "$OUTPUT" "$REMOVED"
 log_counts "$OUTPUT" "Count after cleaning"
 log_counts "$REMOVED" "Count of removed sequences"
-deduplicate_sequences "$OUTPUT" "$OUTPUT_DEDUP" "$REMOVED_DEDUP"
-log_counts "$OUTPUT_DEDUP" "Count after deduplication"
-log_counts "$REMOVED_DEDUP" "Count of removed sequences in deduplication"
-
-# Copy the deduplicated sequences to the final output
-cp "$OUTPUT_DEDUP" "$OUTPUT"
-log_counts "$OUTPUT" "Final count"
+sort_and_deduplicate_sequences "$OUTPUT" "$FINAL_OUTPUT"
+log_counts "$FINAL_OUTPUT" "Final count"
 
 # Get the end time and calculate the duration
 END_TIME=$(date +%s)
 DURATION=$((END_TIME - START_TIME))
 echo "Script ended at $(date)" >> "$LOG_FILE"
 echo "Execution time: $DURATION seconds" >> "$LOG_FILE"
+
+
+# deduplicate_sequences() {
+#   local fasta_file=$1
+#   local dedup_file=$2
+  
+#   # Sort sequences by length, SwissProt, and then gene name
+#   local sorted_file=$(mktemp)
+#   sort_fasta_by_gene "$fasta_file" "$sorted_file"
+  
+#   # Initialize previous gene name
+#   local prev_gene=""
+  
+#   # Get total number of lines (for progress bar)
+#   local total_lines=$(wc -l < "$sorted_file")
+#   local line_counter=0
+  
+#   # Process sorted sequences
+#   while IFS= read -r line; do
+#     # Extract gene name from the current sequence
+#     local current_gene=$(echo "$line" | awk -F '|' '{print $5}')
+    
+#     # If the current gene is different from the previous gene, this is a new group
+#     if [[ "$current_gene" != "$prev_gene" ]]; then
+#       # Select the sequence and append it to the output file
+#       echo "$line" >> "$dedup_file"
+      
+#       # Update the previous gene name
+#       prev_gene="$current_gene"
+#     fi
+    
+#     # # Update progress bar
+#     # line_counter=$((line_counter+1))
+#     # if ((line_counter % 1000 == 0)); then
+#     #   echo -ne "Progress: $((100*line_counter/total_lines))%\r"
+#     # fi
+#   done < "$sorted_file"
+#   echo  # Move to the new line after the progress bar
+  
+#   rm "$sorted_file"
+# }
--- linearizefasta.awk
+++
@@ -1,3 +0,0 @@
-/^>/ {printf("%s%s\n",(N>0?"\n":""),$0);N++;next;}
-     {printf("%s",$0);}
-END  {printf("\n");}
